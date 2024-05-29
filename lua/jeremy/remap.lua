001673302017abed7f3de6b9730bb06b30ca143f
Jeremy5909 <jeremyherczeg@gmail.com>
2024-05-28 20:53:52 -0700

    modified:   lua/jeremy/remap.lua

--- lua/jeremy/remap.lua
+++ lua/jeremy/remap.lua
@@ -1,2 +1,32 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- Can move highlighted text with J and K
+vim.keymap.set("v", "J", ":m '>+1<CR>gc=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gc=gv")
+
+vim.keymap.set("n", "J", "mzJ`z") -- better combining
+
+-- better full page jumping
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- better searching
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- unbind evil Q
+vim.keymap.set("n", "Q", "<nop>")
+
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizere<CR>")
+vim.keymap.set("n", "<leader>f", function()
+    vim.lsp.buf.format()
+end)
+
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+-- make current file executable
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
